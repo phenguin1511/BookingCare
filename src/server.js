c7b2dbd2f06aa67b68176c06361ca40eb38b6cd5
Nguyen Phuc <lehoainguyenphuc11b3@gmail.com>
2024-11-01 19:12:13 +0700

    Install CORS for Api

--- src/server.js
+++ src/server.js
@@ -1,23 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
 require('dotenv').config();
 
-let dotenv
 let app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// Middleware để phân tích cú pháp body của yêu cầu
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// CORS configuration
+app.use(cors({
+    origin: true, // Địa chỉ của client (React app)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Các phương thức HTTP được phép
+    credentials: true // Nếu bạn cần gửi cookie
+}));
+
+// Thiết lập view engine
 viewEngine(app);
+
+// Khởi tạo các route
 initWebRoutes(app);
+
+// Kết nối đến cơ sở dữ liệu
 connectDB();
+
 let port = process.env.PORT || 6969;
 
-//PORT == undefined => port = 6969
-
+// Khởi động server
 app.listen(port, () => {
-    console.log("Backend Nodejs is running on the port: " + port)
-})
+    console.log("Backend Node.js is running on the port: " + port);
+});
